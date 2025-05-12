49f474407c47e844bb3f95010fc6a7b14c4961b6
pieakshat <akshat.05p@gmail.com>
2025-05-12 23:46:14 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket 
+import threading 
+
+HEADER = 64
+PORT = 5500 
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+# // param 1 is the family 
+# param2 is the method
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr): 
+    print(f"new connections {addr} connected")
+
+    connected = True 
+    while connected: 
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length: 
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE: 
+                connected = False
+
+        print(f"[{addr}] {msg}")
+        conn.send("Msg received".encode(FORMAT))
+    conn.close()
+
+
+def start(): 
+    server.listen()
+    print(f"[Listening] Server is listening on {SERVER}")
+    while True: 
+        conn, addr = server.accept() 
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("Starting server is starting...")
+start()
+
